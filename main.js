a466ecfccb61060915bed0756bc3e778958385ac
Colin Bagley <cjbagley@googlemail.com>
2024-02-08 10:57:42 +0000

    Process command line argument for url

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("HELLO WORLD!");
+const { argv } = require('node:process');
+const { normaliseURL, getURLsFromHTML } = require('./crawl.js');
+
+function main() {
+    if (argv.length === 2) {
+        console.log("Please provide a url to scrape as a command line argument");
+        return;
+    }
+    if (argv.length > 3) {
+        console.log("Expected one argument, but multiple given");
+        return;
+    }
+    try {
+        url = normaliseURL(argv[2]);
+        console.log(`Starting web crawl for ${argv[2]}`);
+    } catch (error) {
+        console.log(`Error with given URL: ${error.message}`);
+    }
+}
+
+main();
